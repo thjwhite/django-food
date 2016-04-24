0eef0b6ff99cf0a970a93e32b133d3967c8e8d47
TJ White <thjwhite@gmail.com>
2016-04-24 16:36:57 -0700

    Fix directory setup

--- personal_website/personal_website/urls.py
+++ personal_website/personal_website/urls.py
@@ -1,4 +1,4 @@
-"""django_food URL Configuration
+"""personal_website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
--- personal_website/personal_website/wsgi.py
+++ personal_website/personal_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_food project.
+WSGI config for personal_website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_food.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_website.settings")
 
 application = get_wsgi_application()
