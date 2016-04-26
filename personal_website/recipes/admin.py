73a329369d9c9a2fab6dfad948f6793704e08912
TJ White <thjwhite@gmail.com>
2016-04-25 19:51:12 -0700

    customize admin site a bit. add __str__ methods.

--- personal_website/recipes/admin.py
+++ personal_website/recipes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Recipe, IngredientRequirement, Ingredient
 
-# Register your models here.
+admin.site.site_header = 'TJ White\'s website admin panel'
+admin.site.site_title = 'TJ White\'s personal website'
+admin.site.index_title = 'Administration'
+
+
+class IngredientRequirementInline(admin.TabularInline):
+    model = IngredientRequirement
+    extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General Information', {'fields': ['name', 'title']}),
+        ('Publishing Information', {'fields': ['date_published']})
+    ]
+    inlines =[IngredientRequirementInline]
+    search_fields = ['title']
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
--- personal_website/recipes/models.py
+++ personal_website/recipes/models.py
@@ -6,10 +6,16 @@ class Recipe(models.Model):
     title = models.CharField(max_length=255)
     date_published = models.DateTimeField()
 
+    def __str__(self):
+        return self.title
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class IngredientRequirement(models.Model):
@@ -17,3 +23,6 @@ class IngredientRequirement(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
     measurement_unit = models.CharField(max_length=255)
     number_required = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return "[%s %s %s]" % (self.number_required, self.measurement_unit, str(self.ingredient))
